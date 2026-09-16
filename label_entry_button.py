9e8e8198e95633211f2392d32ceff9506b928fe0
VincentEPython <vincent.emelone@zugerklassen.ch>
2026-09-16 15:54:56 +0200

    Enhance label_entry_button with input display and close button
    Added functionality to display user input and close the window.

--- label_entry_button.py
+++ label_entry_button.py
@@ -3,6 +3,15 @@ import tkinter as tk
 # creating a window
 window = tk.Tk()
 
+# setting window dimentions
+window.geometry("700x500")
+
+def display_info():
+    #getting data from entry widget
+    data = name_entry.get()
+    print(f" Hey {data} Welcome to your first App.")
+
+
 #creating a label
 name_label = tk.Label(window,text = "Enter your name") 
 name_label.pack(pady = 20)
@@ -12,10 +21,12 @@ name_entry = tk.Entry(window)
 name_entry.pack(pady = 10)
 
 #creating a button
-submit_button = tk.Button(window,text = " Submit")
+submit_button = tk.Button(window,text = " Submit", command = display_info)
 submit_button.pack(pady = 20)
 
-
+close_button = tk.Button(window,text = " Close", command = window.destroy)
+close_button.pack(pady = 20)
+ 
 # keeps the window open
 window.mainloop()
 
